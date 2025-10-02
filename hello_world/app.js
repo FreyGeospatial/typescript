be63dc7ae9ab4f5ffc0fc44c34def679364807e5
FreyGeospatial <jfrey0725@gmail.com>
2025-10-02 19:54:09 -0400

    hello world

---
+++ hello_world/app.js
@@ -0,0 +1,5 @@
+// to compile, run tsc app.ts
+// to run, use node app.js
+var message = "Hello, TypeScript!";
+console.log(message);
+console.log(message.toLocaleLowerCase());
---
+++ hello_world/app.ts
@@ -0,0 +1,7 @@
+// to compile, run tsc app.ts
+// to run, use node app.js
+
+const message: string = "Hello, TypeScript!";
+
+console.log(message);
+console.log(message.toLocaleLowerCase());
